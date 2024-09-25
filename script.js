75cedaff0b53bfc94a415de030d683ad573a1acf
fishtested <123904088+fishtested@users.noreply.github.com>
2024-09-25 00:59:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,43 @@ if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 }
 updateImages();
 });
+
+function randomProject() {
+    const projects = [
+        1, // Weather Somewhere
+        2, // SimpleIP
+        3, // Comicify
+        4, // SimpleTime
+        5, // RandomWord
+        6, // SoundButtonWebsiteThing
+        7, // Cat Facts
+        8, // Generate a Passphrase
+    ];
+    project = projects[Math.floor(Math.random() * projects.length)];
+
+    if (project == "1") {
+        window.location.href='https://weather.dino.icu'
+    }
+    if (project == "2") {
+        window.location.href='https://simpleip.netlify.app'
+    }
+    if (project == "3") {
+        window.location.href='https://addons.mozilla.org/en-US/firefox/addon/comicify1/'
+    }
+    if (project == "4") {
+        window.location.href='https://simpletime1.netlify.app'
+    }
+    if (project == "5") {
+        window.location.href='https://randomword1.netlify.app'
+    }
+    if (project == "6") {
+        window.location.href='https://soundbuttonwebsitething.netlify.app'
+    }
+    if (project == "7") {
+        window.location.href='https://catfacts1.netlify.app'
+    }
+    if (project == "8") {
+        window.location.href='https://generateapassphrase.netlify.app'
+    }
+
+}
